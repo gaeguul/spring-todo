18203edfe5a773e636091d1e99db68489af57ec0
gaeguul <jeeham0226@gmail.com>
2024-07-09 19:25:33 +0900

    feat: React와 Spring boot 연동

--- src/main/java/com/mysite/todo/HelloController.java
+++ src/main/java/com/mysite/todo/HelloController.java
@@ -1,16 +1,13 @@
 package com.mysite.todo;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
-    @RequestMapping("/hello")
-    @ResponseBody
 
-    public String hello () {
+    @GetMapping("/api/hello")
+    public String hello() {
         return "hello world!";
     }
 }
